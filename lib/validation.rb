3f9f17e5f61bafab0e51da17def2b912d73a43bd
amhursh <amhursh@gmail.com>
2017-07-19 00:08:15 -0600

    Build out more validations

--- lib/validation.rb
+++ lib/validation.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 module Validation
 
   def check_if_coordinates_are_in_board(coordinates)
@@ -6,8 +8,31 @@ module Validation
     (given_coordinates - board_coordinates).empty?
   end
 
-  def check_if_coordinates_are_connected(coordinates)
-    given
+  def check_if_coordinates_are_connected_in_row(coordinates)
+    length = count_coordinates(coordinates)
+    organized = break_down_coordinate_array(coordinates, length)
+    return true if same_row?(organized[1]) && (organized[0].each_cons())
+
+  end
+
+  def consecutive?(array)
+    
+  end
+
+  def same_row?(array)
+    array.uniq.length == 1
+  end
+
+  def count_coordinates(coordinates)
+    coordinate_count = coordinates.split.count
+  end
+
+  def break_down_coordinate_array(coordinates, length)
+    organized = coordinates.gsub(/\s+/, "").split("").sort.each_slice(length).to_a
+    organized[0] = organized[0].map do |num|
+      num.to_i
+    end
+    organized
   end
 
 
--- test/validation_test.rb
+++ test/validation_test.rb
@@ -14,7 +14,7 @@ class ValidationTest < Minitest::Test
 
   def test_that_checks_board_for_spaces
     player = Player.new(4)
-    
+
     assert player.check_if_coordinates_are_in_board("A4 A1")
     refute player.check_if_coordinates_are_in_board("A1 A5")
 
@@ -22,6 +22,32 @@ class ValidationTest < Minitest::Test
 
     assert player2.check_if_coordinates_are_in_board("A4 H8")
     refute player2.check_if_coordinates_are_in_board("A1 A12")
+  end
+
+  def test_coordinate_count
+    player = Player.new(4)
+
+    assert_equal 2, player.count_coordinates("A3 A4")
+    assert_equal 3, player.count_coordinates("A2 A3 A4")
+  end
+
+  def test_broken_down_coordinate_array
+    player = Player.new(4)
+    length = player.count_coordinates("A2 A3 A4")
+
+    assert_equal [[2, 3, 4], ["A", "A", "A"]], player.break_down_coordinate_array("A2 A3 A4", length)
+
+    player2 = Player.new(4)
+    length2 = player.count_coordinates("A3 A4")
+
+    assert_equal [[3, 4], ["A", "A"]], player.break_down_coordinate_array("A3 A4", length2)
+  end
+
+  def test_same_row
+    player = Player.new(4)
+
+    assert player.same_row?(["A", "A", "A"])
+    refute player.same_row?(["A", "A", "B"])
   end
 
 end
